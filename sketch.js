3a156c3ec1fa34dbd1beaf438368b365b5514a45
TIEN TRUONG <TTT21B@FSU.EDU>
2024-09-03 18:16:44 -0400

    yippeee it works

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
 function setup() {
-  createCanvas(400, 400);
+  // i made a canvas that is 600 by 400
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(125);
+  //bluey
+  background(135, 206, 235);
+  //cool sun
+  //x and y are coods of centerpoint, z is height plus width
+  fill("yellow");
+  //color comes before the shape
+  stroke("orange");
+  strokeWeight(20);
+  circle(550, 50, 100);
+  stroke(0);
+  strokeWeight(1);
+  fill("green");
+  rect(0, 200, 600, 200);
+
+  //emoji time
+  textSize(75)
+  text("🌸", 100, 250)
+  text("⛹️", mouseX, mouseY)
+
 }
